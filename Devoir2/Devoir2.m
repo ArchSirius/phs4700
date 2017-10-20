c8f1b939ce4dc8ef828e16b2f5ca1c5177e7d19d
pogc <pierre-olivier.guimond-cataford@polymtl.ca>
2017-10-20 14:50:15 -0400

    fix coup_verification, fix m-a-j valeur Devoir2

--- Devoir2/Devoir2.m
+++ Devoir2/Devoir2.m
@@ -1,25 +1,28 @@
 function [coup, tf, rbf, vbf] = Devoir2(option, rbi, vbi, wbi) 
 
-	deltaT = 0.4;
+	deltaT = 0.01;
 	tf = 0;
 	rbf = rbi;
 	vbf = vbi;
-  coup = -1;
+	coup = -1;
 
-  if option == 1
-    g = 'option1';
-  elseif option == 2
-    g = 'option2';
-  elseif option == 3
-    g = 'option3';
-  else
-    error('Unwanted option exception')
-  end
+  	if option == 1
+  		g = 'option1';
+  	elseif option == 2
+  		g = 'option2';
+  	elseif option == 3
+  		g = 'option3';
+  	else
+  		error('Unwanted option exception')
+  	end
   
-  q = [rbf; vbf; wbi; [0 0 0]]
-	while coup < 5
-    q = SEDRK4t0(q, tf, deltaT, g)
+  	q = [rbf; vbf; wbi; [0 0 0]]
+  
+	while coup < 0
+		coup = coup_verification(rbf)
+		q = SEDRK4t0(q, tf, deltaT, g);
+		rbf = q(1,:);
+		vbf = q(2,:);
 		tf = tf + deltaT
-    q(4) = tf;
-    coup = coup + 1
+    		q(4) = tf
 	end
