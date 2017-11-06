27a55d2cbd58b54dc7405bfcfe42d7555690eb64
pogc <pierre-olivier.guimond-cataford@polymtl.ca>
2017-11-06 17:24:36 -0500

    testing, bcp de valeurs NaN dans compute_break

--- Devoir3/Devoir3.m
+++ Devoir3/Devoir3.m
@@ -1,21 +1,21 @@
 function [Coll tf raf vaf rbf vbf] = Devoir2(rai, vai, rbi, vbi, tb)
-	deltaT = 0.001;
+	deltaT = 0.01;
 	minSpeed = 0.01;
 
 	Coll = 1;
 	tf = 0;
-	raf = [rai; ang_rot(vai)];
-	vaf = vai;
-	rbf = [rbi; ang_rot(vbi)];
-	vbf = vbi;
+	raf = [rai(1); rai(2); ang_rot(vai)];
+	vaf = vai.';
+	rbf = [rbi(1); rbi(2); ang_rot(vbi)];
+	vbf = vbi.';
 
-	qa = [raf; vaf; 1];
-	qb = [rbf; vbf; 2];
+	qa = [raf, vaf, [1; 0; 0]]
+	qb = [rbf, vbf, [2; 0; 0]]
 
 	tic
 	while Coll ~= 0 && (norm([raf(1); raf(2)]) >= minSpeed || norm([rbf(1); rbf(2)]) >= minSpeed)
-		qa = SEDRK4t0(qa, tf, deltaT, 'compute_break');
-		raf(3) = raf(3) + vai(3) * deltaT; 
+		qa = SEDRK4t0(qa, tf, deltaT, 'compute_break')		
+		raf(3) = raf(3) + vai(3) * deltaT;
 		...TODO determine collision
 		if tf < tb
 			g = 'compute_run';
@@ -26,5 +26,13 @@ function [Coll tf raf vaf rbf vbf] = Devoir2(rai, vai, rbi, vbi, tb)
 		qb = SEDRK4t0(qb, tf, deltaT, g);
 		...TODO determine collision
 		tf = tf + deltaT;
+		raf = qa(1,:);
+        	rbf = qb(1,:);
+        	vaf = qa(2,:);
+        	vbf = qb(2,:);
 	end
 	toc
+	raf = qa(1,:)
+	rbf = qb(1,:)
+	vaf = qa(2,:)
+	vbf = qb(2,:)
--- Devoir3/compute_break.m
+++ Devoir3/compute_break.m
@@ -2,20 +2,24 @@ function k1 = compute_break(qi, deltaT)
 	ri = qi(1,:);
 	vi = qi(2,:);
 
-	if qi(3)==1 
+	qi
+
+	if qi(1,3)==1
 		m = 1540;
-	elseif qi(3)==2
+	elseif qi(1,3)==2
 		m = 1010;
 	else
 		error('Unwanted option exception')
 	end
-		
 
-	F = force_friction(m, vi);
-	A = acceleration(F, m);
-	V = velocity([vi(1); vi(2)], deltaT);
-	R = position([ri(1); ri(2)], [V(1); V(2)]; A, deltaT);
-	deltaR = [R(1);R(2)] - [ri(1); ri(2)];
-	deltaV = [V(1);V(2)] - [vi(1); vi(2)];
 
-	k1 = [[deltaR; ri(3)]; [deltaV; vi(3)]; qi(3)];
+	F = force_friction(m, vi)
+	A = acceleration(F, m)
+	V = velocity([vi(1), vi(2)], deltaT, A)
+	R = position([ri(1), ri(2)], [V(1), V(2)], A, deltaT)
+	
+	ri
+
+	deltaR = [R(1)-ri(1),R(2)-ri(2)]
+	deltaV = [V(1),V(2)] - [vi(1), vi(2)]
+	k1 = [[deltaR, ri(3)].'; [deltaV, vi(3)].'; [0; 0; 0].'];
