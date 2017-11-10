05e72965869709c842a55d0f1e7039d5d4697063
pogc <pierre-olivier.guimond-cataford@polymtl.ca>
2017-11-10 12:56:11 -0500

    raf(3), rbf(3)

--- Devoir3/Devoir3.m
+++ Devoir3/Devoir3.m
@@ -2,8 +2,8 @@ function [Coll tf raf vaf rbf vbf] = Devoir2(rai, vai, rbi, vbi, tb)
 	deltaT = 0.01;
 	minSpeed = 0.01;
 
-    lona = [4.78*0.5 1.82*0.5];
-    lonb = [4.23*0.5 1.6*0.5];
+   	lona = [4.78*0.5 1.82*0.5];
+   	lonb = [4.23*0.5 1.6*0.5];
 	Coll = 1;
 	tf = 0;
 	raf = [rai(1); rai(2); ang_rot(vai)];
@@ -14,38 +14,41 @@ function [Coll tf raf vaf rbf vbf] = Devoir2(rai, vai, rbi, vbi, tb)
 	qa = [raf, vaf, [1; 0; 0]];
 	qb = [rbf, vbf, [2; 0; 0]];
     
-    axis manual;
-    axis([-20 120 -20 120]);
+    	axis manual;
+    	axis([-20 120 -20 120]);
 	tic
 	while Coll ~= 0 && (norm([raf(1); raf(2)]) >= minSpeed || norm([rbf(1); rbf(2)]) >= minSpeed)
-        rotated_positionA = rotated_position(raf, lona);
-        rotated_positionB = rotated_position(rbf, lonb);
-        xA = rotated_positionA(:, 1)
-        yA = rotated_positionA(:, 2)
-        if exist('pa')
-            delete(pa);
-            delete(pb);
-        end
-        pa = patch(xA, yA, 'green');
-        
-        xB = rotated_positionB(:, 1);
-        yB = rotated_positionB(:, 2);
-        pb = patch(xB, yB, 'yellow');
-        drawnow;
-        
-        if collision(raf(2), rbf(2), rotated_positionA, rotated_positionB)
+		rotated_positionA = rotated_position(raf, lona);
+		rotated_positionB = rotated_position(rbf, lonb);
+		xA = rotated_positionA(:, 1)
+		yA = rotated_positionA(:, 2)
+		if exist('pa')
+		    delete(pa);
+		    delete(pb);
+		end
+		pa = patch(xA, yA, 'green');
+		
+		xB = rotated_positionB(:, 1);
+		yB = rotated_positionB(:, 2);
+		pb = patch(xB, yB, 'yellow');
+		drawnow;
+		
+		if collision(raf(2), rbf(2), rotated_positionA, rotated_positionB)
 			...TODO VALEURS INSTANT APRES COLLISION
+			raf(2)
+			rbf(2)
 			break; 
 		end
-        
+		
 		qa = SEDRK4t0(qa, tf, deltaT, 'compute_break');	
-		raf(3) = raf(3) + vai(3) * deltaT;
+		raf3 = raf(3) + vai(3) * deltaT;
 		
 		if tf < tb
 			g = 'compute_run';
+			rbf3 = rbf(3);
 		else
 			g = 'compute_break';
-			rbf(3) = rbf(3) + vbi(3) * deltaT;
+			rbf3 = rbf(3) + vbi(3) * deltaT;
 		end
 		qb = SEDRK4t0(qb, tf, deltaT, g);
 		
@@ -53,15 +56,18 @@ function [Coll tf raf vaf rbf vbf] = Devoir2(rai, vai, rbi, vbi, tb)
 	
 		qaPrime = qa.';
 		qbPrime = qb.';
+		
 
 		raf = qaPrime(1,:);
-        rbf = qbPrime(1,:);
-        vaf = qaPrime(2,:);
-        vbf = qbPrime(2,:);
+		raf(3) = raf3;
+		rbf = qbPrime(1,:);
+		rbf(3) = rbf3;
+		vaf = qaPrime(2,:);
+		vbf = qbPrime(2,:);
 	end
 	toc
 	qaPrime = qa.';
-        qbPrime = qb.';
+	qbPrime = qb.';
 	raf = qaPrime(1,:)
 	rbf = qbPrime(1,:)
 	vaf = qaPrime(2,:)
