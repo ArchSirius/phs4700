a7073874c09414c97e48b8c68614b88b7b5bbcdc
Samuel Lavoie-Marchildon <salavb@gigl.polymtl.ca>
2017-11-10 11:15:33 -0500

    Vizualization

--- Devoir3/Devoir3.m
+++ Devoir3/Devoir3.m
@@ -2,6 +2,8 @@ function [Coll tf raf vaf rbf vbf] = Devoir2(rai, vai, rbi, vbi, tb)
 	deltaT = 0.01;
 	minSpeed = 0.01;
 
+    lona = [4.78*0.5 1.82*0.5];
+    lonb = [4.23*0.5 1.6*0.5];
 	Coll = 1;
 	tf = 0;
 	raf = [rai(1); rai(2); ang_rot(vai)];
@@ -11,16 +13,33 @@ function [Coll tf raf vaf rbf vbf] = Devoir2(rai, vai, rbi, vbi, tb)
 
 	qa = [raf, vaf, [1; 0; 0]];
 	qb = [rbf, vbf, [2; 0; 0]];
-
+    
+    axis manual;
+    axis([-20 120 -20 120]);
 	tic
 	while Coll ~= 0 && (norm([raf(1); raf(2)]) >= minSpeed || norm([rbf(1); rbf(2)]) >= minSpeed)
-		qa = SEDRK4t0(qa, tf, deltaT, 'compute_break');	
-		raf(3) = raf(3) + vai(3) * deltaT;
-		
-		if collision(raf, rbf) 
+        rotated_positionA = rotated_position(raf, lona);
+        rotated_positionB = rotated_position(rbf, lonb);
+        xA = rotated_positionA(:, 1)
+        yA = rotated_positionA(:, 2)
+        if exist('pa')
+            delete(pa);
+            delete(pb);
+        end
+        pa = patch(xA, yA, 'green');
+        
+        xB = rotated_positionB(:, 1);
+        yB = rotated_positionB(:, 2);
+        pb = patch(xB, yB, 'yellow');
+        drawnow;
+        
+        if collision(raf(2), rbf(2), rotated_positionA, rotated_positionB)
 			...TODO VALEURS INSTANT APRES COLLISION
 			break; 
 		end
+        
+		qa = SEDRK4t0(qa, tf, deltaT, 'compute_break');	
+		raf(3) = raf(3) + vai(3) * deltaT;
 		
 		if tf < tb
 			g = 'compute_run';
@@ -30,20 +49,15 @@ function [Coll tf raf vaf rbf vbf] = Devoir2(rai, vai, rbi, vbi, tb)
 		end
 		qb = SEDRK4t0(qb, tf, deltaT, g);
 		
-		if collision(raf, rbf) 
-                    	...TODO VALEURS INSTANT APRES COLLISION
-		    	break; 
-                end
-
 		tf = tf + deltaT;
 	
 		qaPrime = qa.';
 		qbPrime = qb.';
 
 		raf = qaPrime(1,:);
-        	rbf = qbPrime(1,:);
-        	vaf = qaPrime(2,:);
-        	vbf = qbPrime(2,:);
+        rbf = qbPrime(1,:);
+        vaf = qaPrime(2,:);
+        vbf = qbPrime(2,:);
 	end
 	toc
 	qaPrime = qa.';
--- Devoir3/collision.m
+++ Devoir3/collision.m
@@ -1,43 +1,10 @@
-function [isCollision] = collision(raf, rbf)
+function [isCollision] = collision(angA, angB, rotatedA, rotatedB)
   ... Angle en radiant selon les axes des x positif
-  maX = raf(1);
-  maY = raf(2);
-  angA = -raf(3);
-
-  mbX = rbf(1);
-  mbY = rbf(2);
-  angB = -rbf(3);
-  lonaX = 4.78 * 0.5;
-  lonaY = 1.82 * 0.5;
-  lonbX = 4.23 * 0.5;
-  lonbY = 1.6 * 0.5;
-  
-  
-  posANotRotated = [-lonaX lonaY ;...
-                      lonaX lonaY ;...
-                      lonaX -lonaY ;...
-                    -lonaX -lonaY ];
-  rotatedA = rotate(angA, posANotRotated) + [maX  maY];
-
-  posBNotRotated = [-lonbX lonbY ;...
-                    lonbX lonbY ;...
-                    lonbX -lonbY ;...
-                    -lonbX -lonbY ];
-  rotatedB = rotate(angB, posBNotRotated) + [mbX  mbY];
-
-  systemA = rotate(-angA, [rotatedA ; rotatedB]);
+  systemA = rotate(angA, [rotatedA ; rotatedB]);
   isCollision = check_collision(systemA);
 
   if not(isCollision)
-    systemB = rotate(-angB, [rotatedB ; rotatedA]);
+    systemB = rotate(angB, [rotatedB ; rotatedA]);
     isCollision = check_collision(systemB);
   end
     
-
-
-
-
-
- 
-
-  
