260dc7c0bb8ce738b8ab160fb92ca3c9f6580a98
Samuel Rondeau <samuel.rondeau@polymtl.ca>
2017-12-03 12:54:49 -0500

    Fix phi_out

--- Devoir4/Refraction.m
+++ Devoir4/Refraction.m
@@ -19,9 +19,8 @@ function [w1, isRefracted] =  Refraction(r, w0, mCylindre, hCylindre, n_in, n_ou
     phi_in = acos(dot(w0_xy,normal_xy) / (norm(w0_xy) * norm(normal_xy) + 1e-10));
     phi_out = asin(n_in * sin(phi_in) / n_out);
   end
-
-  w0_xz = [w0(1) w0(3)]; 
-  normal_xz = [normal(1) normal(3)];
+  w0_xz = [norm(w0(1:2)) w0(3)];
+  normal_xz = [norm(normal(1:2)) normal(3)];
   theta_in = acos(dot(w0_xz,normal_xz) / (norm(w0_xz) * norm(normal_xz) + 1e-10));
   % TODO gerer reflextion total interne
   theta_out = asin(n_in * sin(theta_in) / n_out);
@@ -30,10 +29,7 @@ function [w1, isRefracted] =  Refraction(r, w0, mCylindre, hCylindre, n_in, n_ou
     theta_out = -theta_out;
   end
 
-  angleNormal_xy = atan2(normal(2), normal(1)); 
-  w1 = [sin(theta_out)*cos(phi_out) sin(theta_out)*sin(phi_out) cos(theta_out)];
-  w1 = [w1(1)+cos(angleNormal_xy) w1(2)+sin(angleNormal_xy) w1(3)];
-
+  w1 = [sin(theta_out)*cos(pi-phi_out) sin(theta_out)*sin(pi-phi_out) cos(theta_out)];
   isRefracted = 1;
   if abs(phi_out)>(pi/2) || abs(theta_out)>(pi/2)
 	  isRefracted = 0;
